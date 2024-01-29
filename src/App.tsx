edfc5953939125a9d92705f4d6d70706ae8a0657
GurudeepNaik <gurudeepnaik1999@gmail.com>
2024-01-30 00:00:19 +0530

    shadcn, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  CardFooter,
+  CardHeader,
+  Card,
+  CardContent,
+} from "./components/ui/card";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-background text-foreground w-full h-full">
+      <Card>
+        <CardHeader>Hello</CardHeader>
+        <CardContent>Hello 2</CardContent>
+        <CardFooter>Hello</CardFooter>
+        <Button>Hello</Button>
+      </Card>
     </div>
   );
 }
